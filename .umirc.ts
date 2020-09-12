d1f193474884caedf92298353aaa5147a2c53638
xiaoyu11111 <1848051540@qq.com>
2020-09-12 15:40:48 +0800

    fix:部署项目

--- .umirc.ts
+++ .umirc.ts
@@ -4,6 +4,9 @@ export default defineConfig({
   favicon: '/assets/xiaoyu.svg',
   logo: '/imges/xiaoyu.svg',
   outputPath: 'docs-dist',
+  base: '/XYDesignReact',
+  publicPath: '/XYDesignReact/',
+  exportStatic: {},
   mode: 'site',
   // more config: https://d.umijs.org/config
 });
--- config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  base: '/XYDesignReact',
-  publicPath: '/XYDesignReact/',
-  exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
-  // 其他配置
-};
