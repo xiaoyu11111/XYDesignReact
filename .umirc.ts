1d56324fad0a912c29d91d4110e7cb9987d70808
xiaoyu11111 <1848051540@qq.com>
2020-09-12 16:37:41 +0800

    feat:切换图片地址

--- .umirc.ts
+++ .umirc.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   title: 'XY Design React',
   favicon: './assets/xiaoyu.svg',
-  logo: './imges/xiaoyu.svg',
+  logo: './images/xiaoyu.svg',
   outputPath: 'docs-dist',
   base: '/XYDesignReact',
   publicPath: '/XYDesignReact/',
